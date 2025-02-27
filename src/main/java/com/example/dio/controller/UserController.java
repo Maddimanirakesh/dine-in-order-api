2a7c2fa502f7f2929688b874279a9f7f3ce67810
Maddimanirakesh <rakesh25032002@gmail.com>
2025-02-28 00:33:30 +0530

    Adding UserController

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,8 @@ public class UserController {
         user =userService.registerUser(user);
         return ResponseBuilder.success(HttpStatus.CREATED, "User Created","user");
     }
+
+//    @GetMapping("/users/{userId}")
+//    public ResponseEntity<ResponseStructure<String>> findById()
 
 }
