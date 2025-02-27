51c134d6bce81ccb2b7834eba634eb77cc4225db
Maddimanirakesh <rakesh25032002@gmail.com>
2025-02-27 17:54:11 +0530

    Adding UserServiceImpl and Implementing the registerUser method

---
+++ src/main/java/com/example/dio/model/User.java
@@ -0,0 +1,42 @@
+package com.example.dio.model;
+
+import com.example.dio.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "users")
+@Inheritance(strategy =InheritanceType.JOINED)
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name ="user_id")
+    private long userId;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name ="email")
+    private String email;
+
+    @Column(name ="password")
+    private String password;
+
+    @Column(name ="ph_no")
+    private String phNo;
+
+    @Column(name ="role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name ="created_at")
+    private LocalDate createdAt;
+
+    @Column(name ="last_modified_at")
+    private LocalDate lastModifiedAt;
+}
---
+++ src/main/java/com/example/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.dio.service.impl;
+
+import com.example.dio.enums.UserRole;
+import com.example.dio.model.Admin;
+import com.example.dio.model.Staff;
+import com.example.dio.model.User;
+import com.example.dio.repository.UserRepository;
+import com.example.dio.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User registerUser(User user) {
+        User user2 = this.createUserByRole(user.getRole());
+
+        this.mapToNewUser(user, user2);
+        return userRepository.save(user2);
+    }
+
+    private User createUserByRole(UserRole role) {
+        User user;
+        switch (role){
+            case ADMIN -> user =new Admin();
+            case STAFF -> user =new Staff();
+            default -> throw new RuntimeException("Failed To Register User, Invalid user");
+        }
+        return user;
+    }
+
+
+    private void mapToNewUser(User user, User user2) {
+        user2.setUsername(user.getUsername());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setPhNo(user.getPhNo());
+        user2.setRole(user.getRole());
+        user2.setCreatedAt(user.getCreatedAt());
+        user2.setLastModifiedAt(user.getLastModifiedAt());
+    }
+
+
+
+}
